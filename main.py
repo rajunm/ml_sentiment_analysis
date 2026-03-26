066d9f300298b15d9f871142782c539dacb13d19
mulawade <mulawade@hs-worms.de>
2026-03-26 10:36:12 +0100

    Modified the files to make sure the models are downloaded in the project directory under 'hf_cache' folder

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from dotenv import load_dotenv
 import os
+
+# This looks for the .env file and sets the variables
+load_dotenv()
+
 from flask import Flask, request, jsonify, render_template
 from transformers import pipeline
 
